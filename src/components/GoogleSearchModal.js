42fc4af6b541c30a0fdb809c35e721ef2b6c9c5f
Nathan <nathane1726@gmail.com>
2021-03-22 18:50:52 +0000

    Version 2

---
+++ src/components/GoogleSearchModal.js
@@ -0,0 +1,93 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useSpring, animated } from "react-spring/web.cjs"; // web.cjs is required for IE 11 support
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import GoogleSearchBar from "./GoogleSearchBar";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "top",
+    justifyContent: "center",
+    paddingTop: "40px",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
+
+export default function GoogleSearchModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <IconButton aria-label="delete" disableRipple={true} onClick={handleOpen}>
+        <ExpandMoreIcon />
+      </IconButton>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <GoogleSearchBar />
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
--- src/iphone/index.js
+++ src/iphone/index.js
@@ -1,123 +1,210 @@
-import { Component } from 'react'
-import styles from './iphone.module.css'
+import { Component, Fragment } from "react";
+import styles from "./iphone.module.css";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import SettingsIcon from '@material-ui/icons/Settings';
 
-//import Search from '../component/SearchBar'
-import CardList from '../components/CardList'
-
-import { IoMdSettings } from 'react-icons/io'
+import CardList from "../components/CardList";
+import GoogleSearchModal from "../components/GoogleSearchModal";
 
 export default class Iphone extends Component {
+  state = { location: "Choose a location" };
 
-    state = {
-        city: 'X',
-        units: '',
-        time: '00:00',
-        weather: 'X',
-        temp: '',
-        feelsLike: '0',
-        pressure: '0',
-        humidity: '0',
-        minTemp: '0',
-        maxTemp: '0',
-        windSpeed: '0',
-        seaLevel: '0',
-        clouds: '',
-        forecast: [
-            { weather: "X", temp: 0, precip: 0 },
-            { weather: "X", temp: 0, precip: 0 },
-            { weather: "X", temp: 0, precip: 0 },
-            { weather: "X", temp: 0, precip: 0 },       
-          ]
-    }
+  constructor(props) {
+    super(props);
+    var d = new Date();
+    var t = d.getHours() + ":" + d.getMinutes();
+    this.state = {
+      time: t,
+    };
+  }
 
-    fetchCurrentWeatherData = async () => {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=portsmouth&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
-        await fetch(url)
-            .then(res => res.json())
-            .then(data => this.updateState(data))
-    }
+  componentDidMount() {
+    const location = localStorage.getItem("location");
+    this.setState({ city: location });
+    // Timer for clock
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
 
-    fetchHourlyWeatherData = async () => {
-        const url = `http://api.openweathermap.org/data/2.5/onecall?lat=50.81957441096404&lon=-1.0890056435577038&exclud=current,minutely,daily,alerts&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
-        await fetch(url)
-            .then(res => res.json())
-            .then(data => this.updateList(data))
-    }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
 
-    updateList = (res) => {
-        var list = []
-        var dataLength = 4
-        for (var i = 0; i < dataLength; i++) {
-            // Create a new JavaScript Date object based on the timestamp
-            // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-            var date = new Date(res.hourly[i].dt * 1000);
-            // Hours part from the timestamp
-            var hours = date.getHours();
-            // Minutes part from the timestamp
-            var minutes = "0" + date.getMinutes();
-            const t = {weather: res.hourly[i].weather[0].main, temp: Math.ceil(res.hourly[i].temp), precip: Math.ceil(res.hourly[i].pop), time: (hours + ":" + minutes)};
-            list.push(t)
-        }
-        this.setState({
-            forecast: list
-        })
-    }
+  tick() {
+    var d = new Date();
+    var t = d.getHours() + ":" + d.getMinutes();
+    this.setState({
+      time: t,
+    });
+  }
 
-    updateState = (res) => {
-        this.setState({
-            city: res.name,
-            weather: res.weather[0].main,
-            temp: Math.ceil(res.main.temp),
-            feelsLike: Math.ceil(res.main.feels_like),
-            pressure: Math.ceil(res.main.pressure),
-            humidity: Math.ceil(res.main.humidity),
-            minTemp: Math.ceil(res.main.temp_min),
-            maxTemp: Math.ceil(res.main.temp_max),
-            seaLevel: Math.ceil(res.main.sea_level),
-            windSpeed: Math.ceil(res.wind.speed),
-            clouds: res.clouds.all
-        })
-    }
-
-    // Calls functions when the app has mounted
-    componentDidMount() {
-        this.fetchHourlyWeatherData();
-        this.fetchCurrentWeatherData();
-    }
-
-    render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.header}>
-                    <div className={styles.title}>
-                        <p>{this.state.city}</p>
-                    </div>
-                    <div className={styles.time}>{this.state.time}</div>
-                    <div className={styles.settings}><IoMdSettings /></div>
-                </div>
-                <div className={styles.day}>
-                    <h2>Today</h2>
-                    <h2>Next 5 days</h2>
-                </div>
-                <div className={styles.main}>
-                    <h1>It's {this.state.weather}</h1>
-                    <h3>Temp: {this.state.temp}°</h3>
-                    <h4>Feels like: {this.state.feelsLike}°</h4>
-                </div>
-                <div className={styles.extra}>
-                    <div className={styles.wind}>Wind: {this.state.windSpeed} m/s</div>
-                    <div className={styles.level}>Sea Level: {this.state.seaLevel}</div>
-                    <div className={styles.minT}>Min: {this.state.minTemp}°</div>
-                    <div className={styles.maxT}>Max: {this.state.maxTemp}°</div>
-                </div>
-                <div className={styles.boxContainer}>
-                    <div className={styles.box}></div>
-                    <CardList forecast={this.state.forecast}/>
-                </div>
-                <button onClick={() => this.fetchCurrentWeatherData()}>Test Current</button>
-                <button onClick={() => this.fetchHourlyWeatherData()}>Test Hourly</button>
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            <GoogleSearchModal />
+            <p>{this.state.city}</p>
+          </div>
+          <div className={styles.time}>{this.state.time}</div>
+          <div className={styles.settings}>
+            <SettingsIcon />
+          </div>
+        </div>
+        <div className={styles.day}>
+          {/* <NavTabs />
+           */}
+          <BrowserRouter>
+            <div className="App">
+              <Route
+                path="/"
+                render={({ location }) => (
+                  <Fragment>
+                    <Tabs value={location.pathname}>
+                      <Tab label="Today" component={Link} to="/" />
+                      <Tab
+                        label="Next 5 Days"
+                        component={Link}
+                        to="/forecast"
+                      />
+                    </Tabs>
+                    <Switch>
+                      <Route path="/forecast" render={() => <Forecast />} />
+                      <Route
+                        path="/"
+                        render={() => <Today state={this.state} />}
+                      />
+                    </Switch>
+                  </Fragment>
+                )}
+              />
             </div>
-        )
-    }
+          </BrowserRouter>
+        </div>
+      </div>
+    );
+  }
+}
 
+class Today extends Component {
+  state = {
+    city: "X",
+    units: "",
+    time: "00:00",
+    weather: "X",
+    temp: "",
+    feelsLike: "0",
+    pressure: "0",
+    humidity: "0",
+    minTemp: "0",
+    maxTemp: "0",
+    windSpeed: "0",
+    seaLevel: "0",
+    clouds: "",
+    forecast: [
+      { weather: "X", temp: 0, precip: 0 },
+      { weather: "X", temp: 0, precip: 0 },
+      { weather: "X", temp: 0, precip: 0 },
+      { weather: "X", temp: 0, precip: 0 },
+    ],
+  };
+
+  fetchCurrentWeatherData = async () => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=portsmouth&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.updateState(data));
+  };
+
+  fetchHourlyWeatherData = async () => {
+    const url = `http://api.openweathermap.org/data/2.5/onecall?lat=50.81957441096404&lon=-1.0890056435577038&exclud=current,minutely,daily,alerts&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.updateList(data));
+  };
+
+  updateList = (res) => {
+    var list = [];
+    var dataLength = 4;
+    for (var i = 0; i < dataLength; i++) {
+      // Create a new JavaScript Date object based on the timestamp
+      // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+      var date = new Date(res.hourly[i].dt * 1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      const t = {
+        weather: res.hourly[i].weather[0].main,
+        temp: Math.ceil(res.hourly[i].temp),
+        precip: Math.ceil(res.hourly[i].pop),
+        time: hours + ":" + minutes,
+      };
+      list.push(t);
+    }
+    this.setState({
+      forecast: list,
+    });
+  };
+
+  updateState = (res) => {
+    this.setState({
+      city: res.name,
+      weather: res.weather[0].main,
+      temp: Math.ceil(res.main.temp),
+      feelsLike: Math.ceil(res.main.feels_like),
+      pressure: Math.ceil(res.main.pressure),
+      humidity: Math.ceil(res.main.humidity),
+      minTemp: Math.ceil(res.main.temp_min),
+      maxTemp: Math.ceil(res.main.temp_max),
+      seaLevel: Math.ceil(res.main.sea_level),
+      windSpeed: Math.ceil(res.wind.speed),
+      clouds: res.clouds.all,
+    });
+  };
+
+  // Calls functions when the app has mounted
+  componentDidMount() {
+    this.fetchHourlyWeatherData();
+    this.fetchCurrentWeatherData();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={styles.main}>
+          <h2>It's {this.state.weather}</h2>
+          <h1>{this.state.temp}°</h1>
+          <h4>Feels like {this.state.feelsLike}°</h4>
+        </div>
+        <div className={styles.extra}>
+          <div className={styles.level}>Sea Level: {this.state.seaLevel}</div>
+          <div className={styles.minT}>Min: {this.state.minTemp}°</div>
+          <div className={styles.maxT}>Max: {this.state.maxTemp}°</div>
+          <div className={styles.wind}>Wind: {this.state.windSpeed} m/s</div>
+        </div>
+        <div className={styles.boxContainer}>
+          <div className={styles.box}></div>
+          <CardList forecast={this.state.forecast} />
+        </div>
+        {/* <button onClick={() => this.fetchCurrentWeatherData()}>
+          Test Current
+        </button>
+        <button onClick={() => this.fetchHourlyWeatherData()}>
+          Test Hourly
+        </button> */}
+      </div>
+    );
+  }
+}
+
+class Forecast extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Forecast</h2>
+      </div>
+    );
+  }
 }
